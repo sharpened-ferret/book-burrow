e50270241270a2017578a7ddc59dc6ed298c5316
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-06 16:36:59 +0100

    Added initial book model

---
+++ database/migrations/2023_05_06_152836_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('isbn');
+            $table->text('title');
+            $table->text('author');
+            $table->text('release_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
