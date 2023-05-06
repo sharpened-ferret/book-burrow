074eaf4ce0746294dc2308d8ecf5ad7a4fe28d1f
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-06 16:25:48 +0100

    Added comment and post models

---
+++ database/migrations/2023_05_06_151838_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->date('post_date');
+            $table->bigInteger('author_id')->unsigned();
+            $table->bigInteger('post_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
