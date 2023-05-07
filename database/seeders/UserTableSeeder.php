8dc42fe3840590f59890e51de0d9202e1aacc60e
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-07 01:03:43 +0100

    Added user seeding

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $t = new User;
+        $t->name = "Test";
+        $t->email = "test@test.com";
+        $t->password = bcrypt("password");
+        $t->save();
+
+        User::factory()->count(10)->create();
+    }
+}
