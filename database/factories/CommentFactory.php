1fb2531ce4d3dd1ac5c355e25bc2cc52b2c7b8b0
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-07 01:14:25 +0100

    Added comment seeding

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->text(),
+            'post_date' => fake()->date(),
+            
+            'author_id' => fake()->numberBetween(1, \App\Models\User::count()),
+            'post_id' => fake()->numberBetween(1, \App\Models\Post::count())
+        ];
+    }
+}
---
+++ database/seeders/CommentTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $c = new Comment;
+        $c->content = "No it's not.";
+        $c->post_date = Carbon::now();
+        $c->author_id = 1;
+        $c->post_id = 1;
+        $c->save();
+
+        Comment::factory()->count(10)->create();
+    }
+}
