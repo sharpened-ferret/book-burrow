633a52e5788ee3dc2cc9cda930f25f7f8e07103e
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-07 20:46:46 +0100

    Added core comment rendering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,11 +11,17 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <p>{{$post->content}}<br>Author: {{$post->author->name}}<br>Book: {{$post->book->title}}<br>{{$post->post_date}}</p>
-                    @foreach ($post->comments as $comment)
-                        <p>{{$comment->author->name}}</p>
-                    @endforeach
                 </div>
             </div>
+            <hr>
+            @foreach ($post->comments as $comment)
+            <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg text-sm">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <p>{{$comment->content}}<br>Author: {{$comment->author->name}}<br>{{$comment->post_date}}</p>
+                </div>
+            </div>
+            @endforeach
+                
         </div>
     </div>
 
