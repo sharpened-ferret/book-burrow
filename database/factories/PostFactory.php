ee04e776c323345666c167afba9c35c7f80daa4b
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-07 01:09:42 +0100

    Added basic post seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->text(),
+            'post_date' => fake()->date(),
+            
+            # generates random IDs in a range up to the number of users
+            'author_id' => fake()->numberBetween(1, \App\Models\User::count())
+        ];
+    }
+}
---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $p = new Post;
+        $p->content = "This post is very useful";
+        $p->post_date = Carbon::now();
+        $p->author_id = 1;
+        $p->save();
+
+        Post::factory()->count(10)->create();
+    }
+}
