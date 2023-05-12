d116e4a087b1c325bcae9c77cb8742791060cc29
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-05-12 15:28:06 +0100

    Added initial like formatting

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,16 +10,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p>{{$post->content}}<br>Author: {{$post->user->name}}<br>Book: {{$post->book->title}}<br>{{$post->post_date}}</p>
-                    <div><p>Likes {{$post->likedBy->count()}}</p></div>
+                    <p>{{$post->content}}</p>
+                    <p>Book: {{$post->book->title}}</p>
+                    <div class="mt-2 flex flex-row">
+                        <div class="basis-11/12">
+                            <a class="no-underline hover:underline" href="{{route('users.show', ['id' => $post->user->id])}}">{{$post->user->name}}</a>
+                            <p>{{$post->post_date}}</p>
+                        </div>
+                        <div class="flex flex-row basis-1/12">
+                            <svg class="flex h-8 w-10" fill="none" stroke="currentColor" stroke-width="1.5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"></path>
+                            </svg>
+                            <p>{{$post->likedBy->count()}}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mt-6">Comments:</h1>
             @foreach ($post->comments as $comment)
             <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg text-sm mt-4">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                <p>{{$comment->content}}<br>Author: <a class="no-underline hover:underline" href="{{route('users.show', ['id' => $comment->user->id])}}">{{$comment->user->name}}</a>
-                <p>{{$comment->post_date}}</p>
+                <p>{{$comment->content}}</p>
+                
+                <div class="flex flex-row justify-start" >
+                    <div class="basis-11/12">
+                        <p class="mt-2"><a class="no-underline hover:underline" href="{{route('users.show', ['id' => $comment->user->id])}}">{{$comment->user->name}}</a>
+                        <p>{{$comment->post_date}}</p>
+                    </div>
+                    <div class="flex flex-row basis-1/12">
+                    <svg class="flex h-8 w-10" fill="none" stroke="currentColor" stroke-width="1.5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"></path>
+                    </svg>
+                    <p>{{$comment->likedBy->count()}}</p>
+                    </div>
+                </div>
                 </div>
             </div>
             @endforeach
